1e4d1737ce683685930d5c3bd2055dd060a82c6f
inaie.moscal <inaie.moscal@ipinfusion.com>
2025-11-13 16:05:07 -0300

    altering opengym interface creation

--- ddos_opengym.cc
+++ ddos_opengym.cc
@@ -343,16 +343,6 @@ CreateGridPositionAllocator (uint32_t nNodes, double spacing, double offsetX, do
   return allocator;
 }
 
-void ScheduleNextStateRead(double stepTime, Ptr<OpenGymInterface> openGym)
-{
-    if (openGym == nullptr) {
-      NS_LOG_ERROR("ScheduleNextStateRead: openGym is null!");
-      return;
-    }
-    openGym->NotifyCurrentState();  // envia observação e espera ação do Python
-    Simulator::Schedule(Seconds(stepTime), &ScheduleNextStateRead, stepTime, openGym);
-}
-
 int
 main(int argc, char* argv[])
 {
