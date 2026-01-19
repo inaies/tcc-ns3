1560b11397ea788b04b00fd513f655a2b5a859be
inaie.moscal <inaie.moscal@ipinfusion.com>
2026-01-19 15:03:18 -0300

    changing attack to start at 155s

--- ddos_opengym.cc
+++ ddos_opengym.cc
@@ -776,7 +776,7 @@ main(int argc, char* argv[])
       onoff.SetAttribute("OffTime", StringValue("ns3::ConstantRandomVariable[Constant=0]"));
 
       ApplicationContainer attackApp = onoff.Install(attackerNodes);
-      attackApp.Start(Seconds(160.0));
+      attackApp.Start(Seconds(155.0));
       attackApp.Stop(Seconds(190.0));
     }
 
